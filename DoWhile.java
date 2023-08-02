f53a473ca3884e859758baff87908c62072aa3d5
Kausalya <kausalya@qualitythought.in>
2023-08-02 09:09:41 +0530

    Add

---
+++ DoWhile.java
@@ -0,0 +1,19 @@
+package javabasicprgs;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int i = 0;
+		do { 
+	
+	System.out.println("Output");
+	i++;
+      }while (i<3);
+		
+	}
+	
+	
+	
+
+}
